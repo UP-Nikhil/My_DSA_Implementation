3e1b4c013e27c6e4f71256d335f58cc0d389916a
Nikhil Pratap <pratapnikhil118@gmail.com>
2026-07-19 10:40:11 +0530

    base understanding

--- 7_Recursion.java/0_Recursion_Basic/Recursion.java
+++ 7_Recursion.java/0_Recursion_Basic/Recursion.java
@@ -1,17 +1,18 @@
 public class Recursion {
 
-    // basic recursive function
-    static int P_num(int num){
-        if(num == 0){
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+
+    static int factorial(int n) {
+       // Base condition ;
+        if (n == 0) {
             return 1;
         }
-        System.out.println(num);
-        return  P_num(num -1 );
-    }
-    public static void main(String[] args) {
-       // Base condition 
-       // self work 
-       // recursive work;
-        P_num(5);
+        // self work;
+
+        
+        // recusive work;
+        return n * factorial(n - 1);
     }
 }
